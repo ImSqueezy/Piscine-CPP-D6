f8ae54cb3fa784272bd632d7f5ae85171974f7eb
ImSqueezy <ouaallaabdelali1@gmail.com>
2026-02-25 14:03:19 +0000

    finish ex00/

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 *This project has been created as part of the 42 curriculum by aouaalla.*
 
 # Description
-CPP Module 06 follows the series of CPP Modules.
+CPP Module 06 follows the series of CPP Modules. This one focuses on type conversions and simple parsers.
 
 # Instructions
 This project follows the basic compilation of C/C++!
@@ -10,16 +10,17 @@ This project follows the basic compilation of C/C++!
 Make within each exercise and execute each's corresponding program.
 
 ## ex00/
+The first exercice Introduces static methods and type conversions. It is required write a program that takes an argument, parse it and convert it from char, int, float to double.
 
 Usage:
 ```bash
+./prog arg
 ```
 
 ## ex01/
 
 Usage:
 ```bash
-./poly
 ```
 
 ## ex02/
--- ex00/ScalarConverter.cpp
+++ ex00/ScalarConverter.cpp
@@ -20,7 +20,7 @@ void handlePseudoLiteral(std::string str) {
 	std::cout << "In handle pseudo literal" << std::endl;
 }
 
-void	convertChar(std::string str) {
+static void	convertChar(std::string str) {
 	char	type;
 
 	type = str[0];
@@ -31,10 +31,10 @@ void	convertChar(std::string str) {
 		std::cout << "'" << type << "'\n";
 	std::cout << "int: " << static_cast<int>(type) << std::endl;
 	std::cout << "float: " << std::fixed << std::setprecision(1) << static_cast<float>(type) << "f" << std::endl;
-	std::cout << "double: " << std::fixed << std::setprecision(1) << static_cast<double>(type) << "d" << std::endl;
+	std::cout << "double: " << std::fixed << std::setprecision(1) << static_cast<double>(type) << std::endl;
 }
 			
-void	convertInt(std::string str) {
+static void	convertInt(std::string str) {
 	int		type;
 
 	type = std::atoi(str.c_str());
@@ -50,8 +50,69 @@ void	convertInt(std::string str) {
 	else
 		std::cout << "int: " << type << std::endl;
 	std::cout << "float: " << std::fixed << std::setprecision(1) << static_cast<float>(type) << "f" << std::endl;
-	std::cout << "double: " << std::fixed << std::setprecision(1) << static_cast<double>(type) << "d" << std::endl;
+	std::cout << "double: " << std::fixed << std::setprecision(1) << static_cast<double>(type) << std::endl;
 }			
+
+static void	convertFloat(std::string str) {
+	float		type;
+
+	type = std::atof(str.c_str());
+	std::cout << "char: ";
+	if (type < 0 || type > 127)
+		std::cout << "Impossible\n";
+	else if (!std::isprint(type))
+		std::cout << "Non Displayable\n";
+	else
+		std::cout << "'" << static_cast<char>(type) << "'\n";
+	if (type <= INT_MIN || type >= INT_MAX)
+		std::cout << "int: Impossible\n";
+	else
+		std::cout << "int: " << static_cast<int>(type) << std::endl;
+	std::cout << "float: " << std::fixed << std::setprecision(1) << type << "f" << std::endl;
+	std::cout << "double: " << std::fixed << std::setprecision(1) << static_cast<double>(type) << std::endl;
+}
+
+static void	convertDouble(std::string str) {
+	float		type;
+
+	type = std::atof(str.c_str());
+	std::cout << "char: ";
+	if (type < 0 || type > 127)
+		std::cout << "Impossible\n";
+	else if (!std::isprint(type))
+		std::cout << "Non Displayable\n";
+	else
+		std::cout << "'" << static_cast<char>(type) << "'\n";
+	if (type <= INT_MIN || type >= INT_MAX)
+		std::cout << "int: Impossible\n";
+	else
+		std::cout << "int: " << static_cast<int>(type) << std::endl;
+	std::cout << "float: " << std::fixed << std::setprecision(1) << static_cast<float>(type) << "f" << std::endl;
+	std::cout << "double: " << std::fixed << std::setprecision(1) << type << std::endl;
+}
+
+static void convertPl(const std::string& str)
+{
+    std::cout << "char: Impossible" << std::endl;
+    std::cout << "int: Impossible" << std::endl;
+    
+    if (str == "nan" || str == "nanf") {
+        std::cout << "float: nanf" << std::endl;
+        std::cout << "double: nan" << std::endl;
+    }
+    else if (str == "+inf" || str == "+inff") {
+        std::cout << "float: +inff" << std::endl;
+        std::cout << "double: +inf" << std::endl;
+    }
+    else if (str == "-inf" || str == "-inff") {
+        std::cout << "float: -inff" << std::endl;
+        std::cout << "double: -inf" << std::endl;
+    }
+    else if (str == "inf" || str == "inff") {
+        std::cout << "float: inff" << std::endl;
+        std::cout << "double: inf" << std::endl;
+    }
+}
 
 void ScalarConverter::convert(const std::string& str)
 {
@@ -66,15 +127,16 @@ void ScalarConverter::convert(const std::string& str)
 			convertInt(str);
 			break;
 		case TYPE_FLOAT:
-			/* code */
+			convertFloat(str);
 			break;
 		case TYPE_DOUBLE:
-			/* code */
+			convertDouble(str);
 			break;
 		case TYPE_CHAR:
 			convertChar(str);
 			break;
 		case TYPE_PL:
+			convertPl(str);
 			break;
 	
 		default:
